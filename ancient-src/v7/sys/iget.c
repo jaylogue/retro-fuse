993225f8a2a280a264995b70e88cbc77449f3a3b
Jay Logue <jay.logue@gmail.com>
2021-11-05 18:09:40 -0700

    Fixed an appearently ancient bug in v7's maknode() function that results in a leaked inode when there's no room to create the directory entry for a new file.

--- ancient-src/v7/sys/iget.c
+++ ancient-src/v7/sys/iget.c
@@ -291,6 +291,13 @@ maknode(int16_t mode)
 	ip->i_uid = u.u_uid;
 	ip->i_gid = u.u_gid;
 	wdir(ip);
+	/* Fix for an ancient bug that results in a leaked inode when there's no
+	 * room to create the directory entry for a new file. */
+	if (u.u_error != 0) {
+		ip->i_nlink--;
+		iput(ip);
+		return(NULL);
+	}
 	return(ip);
 }
 
